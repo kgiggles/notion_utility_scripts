96c40b8ac3dc7b7a70da5059934a99f8127f18cd
Keith Gilvar <keith@findigs.com>
2024-05-01 13:23:11 -0400

    add oura env vars to develop against

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 NOTION_BEARER_TOKEN = os.environ.get("NOTION_BEARER_TOKEN")
+
+# https://findigs.postman.co/workspace/KWG-Oura-Data~af608085-a79b-4e23-9e5a-b9da08700c83/overview
+OURA_API_TOKEN = os.environ.get("OURA_API_TOKEN")
+OURA_BASE_URL = os.environ.get("OURA_BASE_URL")
+OURA_CLIENT_ID = os.environ.get("OURA_CLIENT_ID")
+OURA_CLIENT_SECRET = os.environ.get("OURA_CLIENT_SECRET")
+
+
 DATABASE_ID = "01f5145b31654dcbb56b7c7d9c20bea4" # TODO update to CENTCOM
 
 _headers = {
@@ -23,12 +31,14 @@ TARGET_LIST = [
 
 app = FastAPI()
 
+
 @app.post("/webhook/")
 async def webhook_endpoint(request: Request):
     data = await request.json()
     # Process your data here (e.g., log it, trigger other actions, etc.)
     print(data)  # Example action: print data to console
     return JSONResponse(status_code=200, content={"message": "Data received"})
+    # TODO need to validate wh subscription: https://cloud.ouraring.com/v2/docs#section/Setup
 
 
 def get_db_data():
